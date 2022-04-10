7daba779db2833995b895adea425ce663bb25615
이승호 <seungho5623@gmail.com>
2022-04-10 15:48:41 +0900

    Mixins Compoenents

---
+++ src/components/AccordionComponents.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <h4>Mixins Components</h4>
+    <div class="card-header" @click="toggle">Featured</div>
+    <div class="card-body" v-show="show">
+      <h5 class="card-title">Special title treatment</h5>
+      <p class="card-text">
+        With supporting text below as a natural lead-in to additional content.
+      </p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Mixins } from 'vue-property-decorator';
+import ToggleComponents from '@/components/ToggleComponents.vue';
+
+@Component({
+  components: {
+    ToggleComponents,
+  },
+})
+export default class AccordionComponents extends Mixins(ToggleComponents) {}
+</script>
+
+<style>
+.card {
+  width: 500px;
+  margin: 10px;
+}
+</style>
